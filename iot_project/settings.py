e9f2346f536a1829cf84bc9e337ea5f26af2af85
Anindam <anib9871@gmail.com>
2025-09-18 17:27:15 +0530

    Add CSRF_TRUSTED_ORIGINS for Railway

--- iot_project/settings.py
+++ iot_project/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 # -------------------------------------------------
 # INSTALLED APPS
 # -------------------------------------------------
